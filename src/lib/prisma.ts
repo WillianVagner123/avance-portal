3c05647af3db48292b1668466adccb5c9f8cf713
WillianVagner123 <wvagners@gmail.com>
2026-01-27 16:46:02 -0300

    púsh

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,14 +1,34 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+  prisma?: PrismaClient;
+  pgPool?: Pool;
 };
+
+function mustEnv(name: string) {
+  const v = process.env[name];
+  if (!v) throw new Error(`Missing env ${name}`);
+  return v;
+}
 
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error", "warn"],
-  });
+  (() => {
+    const pool =
+      globalForPrisma.pgPool ??
+      new Pool({
+        connectionString: mustEnv("DATABASE_URL"),
+      });
+
+    globalForPrisma.pgPool = pool;
+
+    return new PrismaClient({
+      adapter: new PrismaPg(pool),
+      log: ["error", "warn"],
+    });
+  })();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
