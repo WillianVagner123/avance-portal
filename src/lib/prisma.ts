090dce8039b7bbf134cae49aa13046d1959534c0
WillianVagner123 <wvagners@gmail.com>
2026-01-27 16:39:26 -0300

    update prima

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,36 +1,15 @@
 import { PrismaClient } from "@prisma/client";
-import { PrismaPg } from "@prisma/adapter-pg";
-import { Pool } from "pg";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-  pgPool?: Pool;
+  prisma: PrismaClient | undefined;
 };
-
-function mustEnv(name: string) {
-  const v = process.env[name];
-  if (!v) throw new Error(`Missing env ${name}`);
-  return v;
-}
 
 export const prisma =
   globalForPrisma.prisma ??
-  (() => {
-    const pool =
-      globalForPrisma.pgPool ??
-      new Pool({
-        connectionString: mustEnv("DATABASE_URL"),
-        ssl: process.env.DATABASE_URL?.includes("sslmode=require")
-          ? { rejectUnauthorized: false }
-          : undefined,
-      });
+  new PrismaClient({
+    log: ["error", "warn"],
+  });
 
-    globalForPrisma.pgPool = pool;
-
-    return new PrismaClient({
-      adapter: new PrismaPg(pool),
-      log: ["error", "warn"],
-    });
-  })();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
